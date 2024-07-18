14af0996c268662489baef8f159a165bbda67293
Stan Marsh <stanmarsh8867@gmail.com>
2024-07-18 13:36:21 +0530

    Initial commit

---
+++ fuzzing/src/Deposit.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Deposit {
+    address public seller = msg.sender;
+    mapping(address => uint256) public balance;
+
+    function deposit() external payable {
+        balance[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+        uint256 amount = balance[msg.sender];
+        balance[msg.sender] = 0;
+        (bool s,) = msg.sender.call{value: amount}("");
+        require(s, "failed to send");
+    }
+
+    function changeBalance(address depositor, uint256 amount) public {
+        balance[depositor] = amount;
+    }
+}
